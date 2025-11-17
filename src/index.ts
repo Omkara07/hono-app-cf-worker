611299ded3a2f917499fcd48492a1568e14b59fa
Omkara07 <singh.omkara07@gmail.com>
2025-11-18 01:14:03 +0530

    added middleware

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,24 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
+// authentication middleware - middleware in cloudfare workers with hono
+async function authMiddleware(c: any, next: any) {
+  if (c.req.header("Authorization")) {
+    await next();
+  }
+  else {
+    return c.text('Unauthorized', 401);
+  }
+}
+
+// uses this middleware everywhere(all the routes)
+app.use(authMiddleware)
+
+// here c in the context for this request. context -> consists both request and response
 app.get('/', (c) => c.text('Hello, Hono!'))
 
-app.post('/', async (c) => {
+// to use the middleware only in this route
+app.post('/', authMiddleware, async (c) => {
   const body = await c.req.json()
   console.log(body);
   console.log(c.req.header("Authorization"));
